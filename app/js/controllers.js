30b2a91ad2edaacfdbbe2103fb4eda3ffd3af58b
kvindasAB <kvindas@alivebox.com>
2014-06-14 15:54:40 -0700

    Initial import.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,13 @@
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
+        $scope.myname = "Whatever name";
+        $scope.onButtonClick = function(name){
+            console.log("button just clicked." + name);
+        }
+
+
+
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
