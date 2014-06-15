81369180d2b7050a4a6e6f29b73c3532a9b5b766
kvindasAB <kvindas@alivebox.com>
2014-06-14 22:01:08 -0700

    Login integration - working kiva login halfway.

--- app/js/login-controller.js
+++ app/js/login-controller.js
@@ -3,12 +3,31 @@
 /* Controllers */
 
 angular.module('kivame.login.controllers', [])
-  .controller('LoginController', ['$scope', function($scope) {
+  .controller('LoginController', ['$scope', '$window', 'KivaService', function($scope, $window, KivaService) {
 
-        $scope.myname = "Whatever name";
-        $scope.onButtonClick = function(name){
-            console.log("button just clicked." + name);
+        $scope.oauth = {
+            oauthToken: null,
+            oauthTokenSecret: null
+        };
+
+        $scope.onLoginWithKivaClick = function(){
+            console.log("onLoginWithKivaClick...");
+            KivaService.loginKivaToken(function(result){
+                $scope.oauth.oauthToken = result.oauth_token;
+                $scope.oauth.oauthTokenSecret = result.oauth_token_secret;
+
+                $scope.redirectUserToKivaLogin($scope.oauth);
+            });
         }
+
+        $scope.redirectUserToKivaLogin = function(oauthData){
+            console.log("redirectUserToKivaLogin....");
+            console.log(oauthData);
+            KivaService.redirectToKivaLogin(function(){
+
+            }, oauthData);
+        }
+
 
 
 
