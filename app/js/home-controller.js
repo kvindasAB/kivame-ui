b6cf345f6e2ed875be98145e412cee76b5758392
kvindasAB <kvindas@alivebox.com>
2014-06-14 19:27:07 -0700

    Netflix like slides - dynamic.

--- app/js/home-controller.js
+++ app/js/home-controller.js
@@ -5,6 +5,9 @@
 angular.module('kivame.home.controllers', [])
   .controller('HomeController', ['$scope', 'KivaService', function($scope, KivaService) {
 
+        // CONSTANTS
+        $scope.ITEMS_PER_SLIDE = 4;
+
         // ATTRIBUTES
         $scope.kivalist = null;
 
@@ -19,29 +22,32 @@ angular.module('kivame.home.controllers', [])
             console.log("requestLoans...");
             KivaService.getRecommendedLoans(function(result){
                 console.log("getRecommendedLoans:...");
+                $scope.createSlidesPerCategoryList(result);
                 console.log(result);
                 $scope.kivalist = result;
             });
         }
 
+        $scope.createSlidesPerCategoryList = function(argCategoryList){
+            _.forEach(argCategoryList, function(item){
+                console.log("category..." + item);
+               $scope.createSlidesPerCategory(item);
+            });
+        }
+
+        $scope.createSlidesPerCategory = function(argCategory){
+            argCategory.slides = [];
+            var start, end;
+            for(var i = 0; i < argCategory.loans.length; i++){
+                start = i * $scope.ITEMS_PER_SLIDE;
+                end = (i * $scope.ITEMS_PER_SLIDE) + $scope.ITEMS_PER_SLIDE;
+                end = end > argCategory.length ? argCategory.length : end;
+                argCategory.slides.push(argCategory.loans.slice(start, end));
+            }
+        }
+
         $scope.onButtonClick = function(name){
             console.log("button just clicked." + name);
         }
-
-        // Slides test
-
-        $scope.myInterval = 5000;
-          var slides = $scope.slides = [];
-          $scope.addSlide = function() {
-            var newWidth = 600 + slides.length;
-            slides.push({
-              image: 'http://placekitten.com/' + newWidth + '/300',
-              text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-                ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-            });
-          };
-          for (var i=0; i<4; i++) {
-            $scope.addSlide();
-          }
 
   }]);
