7a859d96c4f4c8a4ac6e8b2b7141f26868c66f73
kvindasAB <kvindas@alivebox.com>
2014-06-15 11:25:51 -0700

    Integrate grunt to use rewrite rules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function ( grunt ) {
             }
         ],
       options: {
-        port: 8000,
+        port: 8080,
         base: 'app',
         hostname: 'localhost'
       },
